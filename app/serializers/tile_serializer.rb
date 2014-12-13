e6e188cd0a6917f5b6b16e0363a5a8bb9107d8c3
Anthony <anthony@anthonymace.com>
2014-12-13 15:51:44 +1300

    serializer trial and error

--- app/serializers/tile_serializer.rb
+++ app/serializers/tile_serializer.rb
@@ -1,14 +1,14 @@
 class TileSerializer < ActiveModel::Serializer
-  attributes :id, :board_id, :x, :y, :right_neighbour
+  attributes :id, :board_id, :x, :y #, :right_neighbour
 
-  def right_neighbour
-  	right_neighbour = object.right_neighbour
-  	if right_neighbour
-  		{ x: right_neighbour.x, y: right_neighbour.y }
-  	else
-  		{ x: nil, y: nil }
-  	end
-  end
-
+  # def right_neighbour
+  # 	right_neighbour = object.right_neighbour
+  # 	if right_neighbour
+  # 		{ x: right_neighbour.x, y: right_neighbour.y }
+  # 	else
+  # 		{ x: nil, y: nil }
+  # 	end
+  # end
+  # attribute :x, :key => :serverX
   # belongs_to :board
 end
