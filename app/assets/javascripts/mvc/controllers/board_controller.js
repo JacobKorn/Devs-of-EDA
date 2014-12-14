b1ab7022e6b6c9d3ac2d0143e665ea443aea2af3
Anthony <anthony@anthonymace.com>
2014-12-14 14:47:47 +1300

    fixing json and selecting tile on server on click

--- app/assets/javascripts/mvc/controllers/board_controller.js
+++ app/assets/javascripts/mvc/controllers/board_controller.js
@@ -7,14 +7,20 @@ var BoardController = function() {
 
 BoardController.prototype = {
 	start: function() {
-		this.renderBoard()
+		this.renderBoard(this.view.renderBoard)
 	},
-	renderBoard: function(){
-		this.board.getServerTiles();
-		console.log(this.board.serverTiles)
-		this.board.makeHexagons();
-		this.view.renderBoard();
-		this.view.addClickEvents(this.board.hexagons)
+	renderBoard: function(callback){
+		// this.board.getServerTiles();
+		// console.log("from controller render board---", this.board.serverTiles)
+		this.board.getServerTiles()
+			.done(this.board.saveTiles.bind(this.board))
+			.done(this.board.makeHexagons.bind(this.board))
+			.done(this.view.renderBoard)
+			.done(this.view.addClickEvents.bind(this.view))
+		// this.board.makeHexagons();
+
+		
+		// this.view.addClickEvents(this.board.hexagons);
 	}
 
 };
