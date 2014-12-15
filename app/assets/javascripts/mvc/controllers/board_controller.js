ce9c565ed13bf0da5d2e30d4f195d3d05b59ca90
Anthony <anthony@anthonymace.com>
2014-12-15 16:42:32 +1300

    resource counters working

--- app/assets/javascripts/mvc/controllers/board_controller.js
+++ app/assets/javascripts/mvc/controllers/board_controller.js
@@ -9,11 +9,20 @@ BoardController.prototype = {
 	start: function() {
 		this.renderBoard(this.view.renderBoard)
 	},
-	renderBoard: function(callback){
+	renderBoard: function(){
 		this.board.getServerTiles()
 			.done(this.board.saveTiles.bind(this.board))
 			.done(this.board.makeHexagons.bind(this.board))
 			.done(this.view.renderBoard)
 			.done(this.view.addClickEvents.bind(this))
+	},
+	logClick: function(event) {
+		var controller = this
+		this.board.logClick(event)
+			.done(this.board.updateResources.bind(this.board))
+			.done(this.updateResources.bind(this));
+	},
+	updateResources: function() {
+		this.view.updateResources(this.board.resources);
 	}
 };
